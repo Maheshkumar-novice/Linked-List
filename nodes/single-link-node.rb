0327ed8ec97083b3da434f33d150e8d76d47bf48
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-23 10:07:52 +0530

    Add head and tail with initialize linked list

--- nodes/single-link-node.rb
+++ nodes/single-link-node.rb
@@ -5,8 +5,8 @@
 class SinglyLinkedListNode
   attr_accessor :value, :next_node
 
-  def initialize
-    @value = nil
-    @next_node = nil
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
 end
--- singly-linked-list.rb
+++ singly-linked-list.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative 'nodes/single-link-node'
+
+# Singly Linked List
+class SinglyLinkedList
+  attr_accessor :head, :tail
+
+  def initialize(value = nil, next_node = nil)
+    @head = SinglyLinkedListNode.new(value, next_node)
+    @tail = @head
+  end
+end
+
