d08cedd061500e62ad1c7ef83c6860d6893fceec
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-23 10:39:16 +0530

    Add #size to singly-linked-list

--- singly-linked-list.rb
+++ singly-linked-list.rb
@@ -6,19 +6,29 @@ require_relative 'nodes/single-link-node'
 # Singly Linked List
 class SinglyLinkedList
   attr_accessor :head, :tail
+  attr_reader :size
 
   def initialize(value = nil, next_node = nil)
     @head = SinglyLinkedListNode.new(value, next_node)
     @tail = @head
+    @size = 1
   end
 
   def append(value)
     @tail.next_node, @tail = SinglyLinkedListNode.new(value)
+    increase_size
   end
 
   def prepend(value)
     new_node = SinglyLinkedListNode.new(value)
     new_node.next_node = @head
     @head = new_node
+    increase_size
+  end
+
+  private
+
+  def increase_size
+    @size += 1
   end
 end
