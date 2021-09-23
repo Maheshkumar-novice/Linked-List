88236358043aee7769d33aa8950e6eb4f77a5ab8
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-23 10:02:20 +0530

    Add single-link-node

--- nodes/single-link-node.rb
+++ nodes/single-link-node.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# Singly Linked List Node
+class SinglyLinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize
+    @value = nil
+    @next_node = nil
+  end
+end
